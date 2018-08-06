2e553394abfeec080f24312fa6f7f9f534c6b200
Renhe Li <renhel@microsoft.com>
2018-08-06 16:48:41 +0800

    modify gitignore

--- IoTSolutionBuilder/Program.cs
+++ IoTSolutionBuilder/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace IoTSolutionBuilder
+namespace Microsoft.VSChina.IoTSolutionBuilder
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
