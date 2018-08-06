8fd1f9e1e8e10cb85930ab0374072cc635ac1314
Renhe Li <renhel@microsoft.com>
2018-08-06 14:14:49 +0800

    Update the solution to use dotnet core

--- IoTSolutionBuilder/Program.cs
+++ IoTSolutionBuilder/Program.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace IoTSolutionBuilder
 {
@@ -11,7 +7,6 @@ namespace IoTSolutionBuilder
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
         }
     }
 }
